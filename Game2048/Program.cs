f220d469881238ae26a5da47bc743b170784432c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-12-05 21:55:54 +0100

    Fix css loading

--- Game2048/Program.cs
+++ Game2048/Program.cs
@@ -2,8 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+app.MapStaticAssets();
 
 app.MapFallbackToFile("index.html");
 
